e7009756ad0cf9f4ce3b041585cca019325fcd7d
Matt Freels <matt@freels.name>
2012-02-28 00:03:57 -0800

    add back redis-specific decimal int decoding

--- src/test/scala/com/twitter/finagle/parser/BenchmarkSpec.scala
+++ src/test/scala/com/twitter/finagle/parser/BenchmarkSpec.scala
@@ -1,7 +1,7 @@
 package com.twitter.finagle.parser.test
 
 import org.specs.Specification
-import org.jboss.netty.buffer.ChannelBuffers
+import org.jboss.netty.buffer.{ChannelBuffers, ChannelBuffer}
 import com.twitter.finagle.parser.incremental._
 import com.twitter.finagle.parser.util._
 
@@ -10,12 +10,35 @@ object Redis {
   import Parsers._
   import DecodingHelpers._
 
-  val skipCRLF = skipBytes(2)
-  val readInt = withRawBuffer { b =>
-    val int = decodeDecimalInt(b)
-    b.skipBytes(2)
-    int
+  def decodeInt(buf: ChannelBuffer): Int = {
+    var sign     = 1
+    var result   = 0
+
+    var c = buf.readByte
+
+    if (c == '-') {
+      sign   = -1
+    } else {
+      result = result * 10 + (c - '0')
+    }
+
+    do {
+      c = buf.readByte
+
+      if (c != '\r') {
+        result = result * 10 + (c - '0')
+      } else {
+        buf.readByte // \n
+        return result * sign
+      }
+    } while (true)
+
+    // not reached
+    0
   }
+
+  val skipCRLF       = skipBytes(2)
+  val readInt        = withRawBuffer { decodeInt(_) }
   val readBulk       = readInt >>= { readBytes(_) through skipCRLF }
   val readSingleBulk = "$" >> readBulk
   val readMultiBulk  = "*" >> (readInt >>= { repN(_, readSingleBulk) })
